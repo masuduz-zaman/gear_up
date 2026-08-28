b2a150843c481a8eaa852f9fd5c71e850281cb47
Masuduzzaman <md.santo0583210@gmail.com>
2026-08-29 02:35:30 +0600

    try to fix deployment error

--- app/(dashboard)/dashboard/customer/oreders/[id]/pay/page.tsx
+++ app/(dashboard)/dashboard/customer/oreders/[id]/pay/page.tsx
@@ -4,5 +4,5 @@ type Props = { params: Promise<{ id: string }> }
 
 export default async function PaymentPage({ params }: Props) {
   const { id } = await params
-  return <PaymentForm orderId={id} />
+  return <PaymentForm  />
 }
